f1445bfcb647ffe0dab69b69a2b69407a8ad4985
Yannick Gladow <yannick.gladow@holidaycheck.com>
2019-11-08 14:36:38 +0100

    Sketch basic programm

--- src/Lib.hs
+++ src/Lib.hs
@@ -1,6 +1,60 @@
+{-# LANGUAGE OverloadedStrings #-}
+{-# LANGUAGE FlexibleContexts #-}
+
 module Lib
-    ( someFunc
-    ) where
+  ( someFunc
+  )
+where
+
+import           System.FilePath                ( FilePath )
+import           Data.UUID                      ( UUID )
+import           Network.URL                    ( URL )
+import           Data.Text                      ( Text
+                                                , append
+                                                )
+import           Data.Foldable                  ( traverse_ )
+import           Control.Monad.Except           ( MonadError(..) )
+import           Control.Exception              ( try )
 
 someFunc :: IO ()
 someFunc = putStrLn "someFunc"
+
+
+data NotionSearchRes = NotionSearchRes { imageURL :: URL, insertId :: UUID }
+
+parsingErrorText :: Text -> Text
+parsingErrorText =
+  append
+    "Unfortunately if failed to parse the text in the Image, if you want to try again, replace this text with add_ocr again. Error was: "
+
+class Notion m where
+  searchNotion :: m [ NotionSearchRes ]
+  insertOCR :: Text -> UUID -> m ()
+
+class FS m where
+  saveFile :: URL -> m FilePath
+  deleteFile :: FilePath -> m ()
+  getFile :: FilePath -> m Text
+
+class Tesseract m where
+  ocrFile :: FilePath -> m FilePath
+
+class ErrorLog m where
+  logErr :: Text -> m ()
+
+updateOcrs
+  :: (Notion m, Monad m, FS m, Tesseract m, MonadError Text m, ErrorLog m)
+  => m ()
+updateOcrs = searchNotion >>= traverse_
+  (\searchRes -> catchError
+    (do
+      imagePath <- saveFile $ imageURL searchRes
+      ocrText   <- catchError (getOCR imagePath) (pure . parsingErrorText)
+      deleteFile imagePath
+      insertOCR ocrText $ insertId searchRes
+    )
+    logErr
+  )
+ where
+  getOCR image =
+    ocrFile image >>= (\fileP -> getFile fileP <* deleteFile fileP)
