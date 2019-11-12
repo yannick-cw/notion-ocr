2ae85e1e28db056bf399dc92144f723cf96b9da5
Yannick Gladow <yannick.gladow@holidaycheck.com>
2019-11-12 17:04:56 +0100

    Catch IOErrors

--- src/Tesseract.hs
+++ src/Tesseract.hs
@@ -9,12 +9,14 @@ import           AppM
 import           CliParser
 import           Control.Monad.Reader
 import           Control.Monad.Except
+import           Control.Exception              ( try )
 import           Data.Text                      ( Text
                                                 , pack
                                                 )
 import           System.Process                 ( rawSystem )
 import           System.FilePath.Posix          ( (</>) )
 import           System.Exit                    ( ExitCode(..) )
+import           GHC.IO.Exception               ( ioe_description )
 
 class Tesseract m where
     ocrFile :: FilePath -> m FilePath
@@ -26,8 +28,9 @@ scanFile :: HasTempDir r => FilePath -> ExceptT Text (ReaderT r IO) FilePath
 scanFile imagePath = do
   tmpDirPath <- asks path
   let outFilePath = tmpDirPath </> "out"
-  sysCode <- liftIO $ rawSystem "tesseract" [imagePath, outFilePath]
-  case sysCode of
-    ExitSuccess -> return outFilePath
-    (ExitFailure _) ->
+  sysCode <- liftIO $ try $ rawSystem "tesseract" [imagePath, outFilePath]
+  case (sysCode :: Either IOError ExitCode) of
+    (Left  ioErr      ) -> throwError $ pack $ ioe_description ioErr
+    (Right ExitSuccess) -> return outFilePath
+    (Right (ExitFailure _)) ->
       throwError $ pack $ "OCR for scanning file " ++ imagePath ++ " failed."
