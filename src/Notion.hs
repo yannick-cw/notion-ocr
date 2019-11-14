3456a15cffc11abd1d4fb28c953b754730963199
Yannick Gladow <yannick.gladow@holidaycheck.com>
2019-11-14 14:21:40 +0100

    WIP

--- src/Notion.hs
+++ src/Notion.hs
@@ -1,8 +1,51 @@
+{-# LANGUAGE OverloadedStrings #-}
+{-# LANGUAGE DeriveGeneric #-}
+{-# LANGUAGE DuplicateRecordFields #-}
+{-# LANGUAGE LambdaCase #-}
 module Notion where
 
 import           Data.Text                      ( Text )
 import           Data.UUID                      ( UUID )
 import           AppM
+
+import           Network.Wreq
+import           Network.HTTP.Client            ( createCookieJar
+                                                , Cookie(Cookie)
+                                                )
+import           Control.Monad.Reader
+import           Control.Monad.Except
+import           Data.Aeson.Types               ( emptyObject
+                                                , ToJSON
+                                                , FromJSON(..)
+                                                , sumEncoding
+                                                , unwrapUnaryRecords
+                                                , genericToJSON
+                                                , fieldLabelModifier
+                                                , defaultOptions
+                                                , SumEncoding(..)
+                                                )
+import           Data.Aeson.Lens                ( _Object
+                                                , key
+                                                )
+import           Data.Aeson                     ( toJSON
+                                                , genericParseJSON
+                                                )
+import           Data.Time.Clock
+import           Data.ByteString.Char8          ( pack )
+import           Data.Text                      ( unpack )
+import           Data.Maybe                     ( listToMaybe
+                                                , catMaybes
+                                                , maybeToList
+                                                )
+import           Data.UUID
+import           Control.Lens
+import           Control.Applicative            ( liftA3 )
+import           GHC.Generics
+import           System.Random                  ( randomIO )
+
+
+notionUrl :: String
+notionUrl = "https://www.notion.so/api/v3/"
 
 data NotionSearchRes = NotionSearchRes { imageURL :: Text, insertId :: UUID }
 
@@ -13,5 +56,4 @@ class Notion m where
 instance Notion AppM where
   searchNotion = pure []
   insertOCR _ _ = pure ()
-
 
