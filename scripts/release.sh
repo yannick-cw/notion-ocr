2122a929a35b322f663686fa71710fc6e664acb2
yannick-cw <yannick.gladow@gmail.com>
2019-11-22 15:26:53 +0100

    Cleanup notion-ocr in release

--- scripts/release.sh
+++ scripts/release.sh
@@ -8,11 +8,13 @@ then
   cp .stack-work/dist/x86_64-osx/Cabal-2.4.0.1/build/notion-ocr-exe/notion-ocr-exe ./release/notion-ocr &&\
   cd release &&\
   zip notion-ocr_mac.zip notion-ocr
+  rm notion-ocr
   cd ..
 else
   mkdir release &&\
   cp .stack-work/dist/x86_64-linux/Cabal-2.4.0.1/build/notion-ocr-exe/notion-ocr-exe ./release/notion-ocr &&\
   cd release &&\
   zip notion-ocr_x86_64-linux.zip notion-ocr
+  rm notion-ocr
   cd ..
 fi 
