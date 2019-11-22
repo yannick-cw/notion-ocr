38c27a4ee5d677ee564dfa70ca034254f90f7ff4
yannick-cw <yannick.gladow@gmail.com>
2019-11-22 13:30:01 +0100

    try different travis option

--- scripts/release.sh
+++ scripts/release.sh
@@ -1,14 +1,17 @@
 #!/bin/bash
+
+echo "Running script to create release .zip"
+
 if [ `uname` = "Darwin" ]
 then
   mkdir release  
-  mv .stack-work/dist/x86_64-osx/Cabal-2.4.0.1/build/notion-ocr-exe/notion-ocr-exe ./release/notion-ocr &&\
+  cp .stack-work/dist/x86_64-osx/Cabal-2.4.0.1/build/notion-ocr-exe/notion-ocr-exe ./release/notion-ocr &&\
   cd release &&\
   zip notion-ocr_mac.zip notion-ocr
   cd ..
 else
   mkdir release &&\
-  mv .stack-work/dist/x86_64-linux-tinfo6/Cabal-2.4.0.1/build/notion-ocr-exe/notion-ocr-exe ./release/notion-ocr &&\
+  cp .stack-work/dist/x86_64-linux/Cabal-2.4.0.1/build/notion-ocr-exe/notion-ocr-exe ./release/notion-ocr &&\
   cd release &&\
   zip notion-ocr_x86_64-linux.zip notion-ocr
   cd ..
